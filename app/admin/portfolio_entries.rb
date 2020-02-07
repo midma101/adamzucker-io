1be691a159b1e15ad6d2287cd8cec0124a7a34bc
Adam Zucker <adman634@gmail.com>
2020-02-06 23:16:38 -0500

    added admin

---
+++ app/admin/portfolio_entries.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register PortfolioEntry do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :github_link, :example_type
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :github_link, :example_type]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end
